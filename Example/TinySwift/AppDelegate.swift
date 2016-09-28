d02b70c2adc44798f7a968b23bfc04a08b0ddbb3
Piotr Sochalewski <sochalewski@gmail.com>
2016-09-28 22:52:36 +0200

    Fixed UIViewController.presentAlertController(title:message:completion:) test

---
+++ Example/TinySwift/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TinySwift
+//
+//  Created by Piotr Sochalewski on 09/28/2016.
+//  Copyright © 2016 Piotr Sochalewski. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Example/TinySwift/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  TinySwift
+//
+//  Created by Piotr Sochalewski on 09/28/2016.
+//  Copyright © 2016 Piotr Sochalewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController { }
