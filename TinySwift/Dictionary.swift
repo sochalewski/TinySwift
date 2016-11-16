74cf97fc10851bcbe18e8bace7ea7aff3829111a
Piotr Sochalewski <sochalewski@gmail.com>
2016-11-16 21:54:01 +0100

    Dictionary.shuffle(), Dictionary.shuffled, generic Collection variables

---
+++ TinySwift/Dictionary.swift
@@ -0,0 +1,40 @@
+//
+//  Dictionary.swift
+//  TinySwift
+//
+//  Created by Piotr Sochalewski on 16.11.2016.
+//  Copyright © 2016 Piotr Sochalewski. All rights reserved.
+//
+
+import Foundation
+#if !os(watchOS)
+    import GameplayKit
+#endif
+
+public extension Dictionary {
+    #if !os(watchOS)
+    /// Shuffles the objects in the dictionary. The objects in the dictionary are shuffled based on a Fisher-Yates shuffle.
+    @available(iOS 9.0, tvOS 9.0, *)
+    public mutating func shuffle() {
+        let shuffledKeys = Array(keys).shuffled
+        let shuffledValues = Array(values).shuffled
+        
+        var dictionary = [Key : Value]()
+        
+        for (key, value) in zip(shuffledKeys, shuffledValues) {
+            dictionary[key] = value
+        }
+        
+        self = dictionary
+    }
+    
+    /// Returns a shuffled instance of the dictionary. The objects in the dictionary are shuffled based on a Fisher-Yates shuffle.
+    @available(iOS 9.0, tvOS 9.0, *)
+    public var shuffled: [Key : Value] {
+        var dictionary = self
+        dictionary.shuffle()
+        
+        return dictionary
+    }
+    #endif
+}
