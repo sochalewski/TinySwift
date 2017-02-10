4b9d7835959655c8b3b47cc42af14eb43769ee92
Piotr Sochalewski <sochalewski@gmail.com>
2017-02-10 23:59:27 +0100

    CGAffineTransform.rotationAngle, CGAffineTransform.scaleX, CGAffineTransform.scaleY

---
+++ Example/Tests/CGAffineTransformTests.swift
@@ -0,0 +1,35 @@
+//
+//  CGAffineTransformTests.swift
+//  TinySwift
+//
+//  Created by Piotr Sochalewski on 10.02.2017.
+//  Copyright © 2017 Piotr Sochalewski. All rights reserved.
+//
+
+import XCTest
+
+class CGAffineTransformTests: XCTestCase {
+    
+    var view: UIView!
+    
+    override func setUp() {
+        super.setUp()
+        view = UIView()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        view = nil
+    }
+    
+    func testGetScale() {
+        view.transform = CGAffineTransform(scaleX: 2.0, y: 3.0)
+        XCTAssert(view.transform.scaleX == 2.0)
+        XCTAssert(view.transform.scaleY == 3.0)
+    }
+    
+    func testGetRotation() {
+        view.transform = CGAffineTransform(rotationAngle: 3.0)
+        XCTAssert(view.transform.rotationAngle == 3.0)
+    }
+}
